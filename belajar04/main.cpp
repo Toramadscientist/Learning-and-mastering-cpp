88a8e752445944a00029a47d834898dc352ff40f
toramadscientist <danishnaufal0989@gmail.com>
2026-08-28 19:00:00 +0700

    ngubah sintax

--- belajar04/main.cpp
+++ belajar04/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <array>
 #include <algorithm>
-const size_t seluruharray = 1;
+const size_t seluruharray = 6;
 
 
 void ngurusinAngka (std::array <int, seluruharray> &angka){
@@ -19,19 +19,12 @@ void ngurusinHuruf (std::array <char, seluruharray> &huruf){
 }
 
 int main(){
-	std::array <int, 1> arrayangka = {0};
-	std::array <char, 1> arrayhuruf = {'a'};
-	int angka;
-	char huruf;
+	std::array <int, 6> arrayangka = {4,1,5,3,0,3};
+	std::array <char, 6> arrayhuruf = {'a','d','f','e','c','b'};
 
-	std::cin >> angka;
-	std::cin >> huruf;
-
-	arrayangka : angka;
-	arrayhuruf : huruf;
-
-	/* std::sort (arrayangka.begin(), arrayangka.end());
-	std::sort (arrayhuruf.begin(), arrayhuruf.end()); */
+	std::sort (arrayangka.begin(), arrayangka.end());
+	std::sort (arrayhuruf.begin(), arrayhuruf.end()); 
+	
 	ngurusinAngka(arrayangka);
 	ngurusinHuruf(arrayhuruf); 
 
