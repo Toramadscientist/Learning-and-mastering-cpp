1ddda876f78c735516f7ba41dfd7e92ac6606033
toramadscientist <danishnaufal0989@gmail.com>
2026-08-29 12:34:11 +0700

    nambah fitur pertambahan

--- belajar06/main.cpp
+++ belajar06/main.cpp
@@ -4,16 +4,40 @@
 using namespace std;
 
 int main(){
+	int hasil;
 	char yesno;
+	char mtk;
+	float nilai1, nilai2;
+	float ytta;
 
 	cout << "KALKULATOR SEDERHANA"<< endl;
 	cout << endl;
 
 	cout << "apakah anda ingin mengunakan kalkulator sederhana?? Y/N"<< endl;
 	cin >> yesno;
-	if (yesno == 'Y'){
+	if (yesno == 'Y' | yesno == 'y'){
+		cout << "pilih operator yng ingin di gunakan" << endl;
+		cout << "membagi : / , perkalian : * , perkurangan : - , pertambahan : +" << endl;
+		cout << endl;
 
-	} else if(yesno == 'N'){
+		cout << "contoh: 'pilih operator: +' " << endl;
+		cout << endl;
+
+		cout << "pilih operator: ";
+		cin >> mtk;
+
+		cout << "pilih nilai pertama";
+		cin >> nilai1;
+
+		cout << "pilih nilai kedua";
+		cin >> nilai2;
+
+		if (mtk == '+'){
+			hasil = nilai1 + nilai2;
+			cout << "hasil: "  << hasil << endl;
+		}
+
+	} else if(yesno == 'N' | yesno == 'n'){
 		cout << "anda menolak" << endl;
 	}else {
 		cout << "invalid input" << endl;
