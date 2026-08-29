4b8ef502fa6f5359cae159fa60a88e4a8017fc46
toramadscientist <danishnaufal0989@gmail.com>
2026-08-29 10:20:26 +0700

    ngubah sikit

--- belajar05/main.cpp
+++ belajar05/main.cpp
@@ -5,14 +5,18 @@ using namespace std;
 const size_t reprearrayarray = 5;
 
 void representasi (array <int, reprearrayarray> &angka){
-	for (int &hasil : angka){
-		cout << "nilaimurid: " <<hasil << endl;
+		cout << "nilai bahasa indo     :" <<angka[0] << ": " << &angka << endl;
+		cout << "nilai bahasa ipa      :" <<angka[1] << ": " << &angka << endl;
+		cout << "nilai bahasa agama    :" <<angka[2] << ": " << &angka << endl;
+		cout << "nilai bahasa mtk      :" <<angka[3] << ": " << &angka << endl;
+		cout << "nilai bahasa binggris :" <<angka[4] << ": " << &angka << endl;
 	}
-	cout << endl;
-}
+
 
  int main(){
  	array <int, 5> nilaimurid = {0,0,0,0,0};
  	representasi(nilaimurid);
+
+
  	return 0;
  }
